bffbadfcf7d8d06cdd8660f0db845f7ab1b180ba
Serhiy Korzun <kiberpunk.mail@gmail.com>
2017-12-27 11:06:17 +0200

    Changed Currencies pseudomodel, added GetCurrencyById method

--- Currencies.go
+++ Currencies.go
@@ -4,6 +4,9 @@ type Currency struct {
     Title string
     Code  string
     Id    uint8
+    IsCoin bool
+    IsToken bool
+    Confirmations uint8
 }
 
 var AcceptedCurrencies = []Currency{
@@ -11,10 +14,25 @@ var AcceptedCurrencies = []Currency{
         Title: "Yuan",
         Code:  "CNY",
         Id:    1,
+        IsCoin: false,
+        IsToken: false,
     },
     {
         Title: "Bitcoin",
         Code:  "BTC",
         Id:    2,
+        IsCoin: true,
+        IsToken: false,
+        Confirmations: 3,
     },
+}
+
+func GetCurrencyById(id uint8) (*Currency) {
+    for _, cur := range AcceptedCurrencies {
+        if cur.Id == id {
+            return &cur
+        }
+    }
+
+    return nil
 }
