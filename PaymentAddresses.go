7a82c515b81318b58f5696107dd3df0e11c190aa
Stanislav Cherviakov <stchervyakov@gmail.com>
2018-01-12 14:12:47 +0200

    Added ExpectedTag to PaymentAddress

--- PaymentAddresses.go
+++ PaymentAddresses.go
@@ -1,19 +1,20 @@
 package exmodels
 
 import (
-    "time"
+	"time"
 )
 
 type PaymentAddresses struct {
-    Id       uint   `gorm:"primary_key"`
-    Address  string `gorm:"size:255" sql:"default: null"`
-    Currency uint8
-    Secret   string `gorm:"size:255" sql:"default: null"`
+	Id          uint   `gorm:"primary_key"`
+	Address     string `gorm:"size:255" sql:"default: null"`
+	Currency    uint8
+	Secret      string `gorm:"size:255" sql:"default: null"`
+	ExpectedTag string `gorm:"size:255" sql:"default: null"`
 
-    CreatedAt *time.Time `sql:"default: null"`
-    UpdatedAt *time.Time `sql:"default: null"`
+	CreatedAt *time.Time `sql:"default: null"`
+	UpdatedAt *time.Time `sql:"default: null"`
 
-    //Account      Accounts                                          //belongs_to
-    AccountId    uint                                              //belongs_to
-    //Transactions []PaymentTransactions `gorm:"ForeignKey:address"` //has many
+	//Account      Accounts                                          //belongs_to
+	AccountId uint //belongs_to
+	//Transactions []PaymentTransactions `gorm:"ForeignKey:address"` //has many
 }
