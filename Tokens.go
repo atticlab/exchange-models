6fbcd1ae9250a9fd942477ac20104c5b014ff27d
ihor <for0work0@gmail.com>
2018-01-19 15:27:26 +0200

    add new token type and request fields to tokens

--- Tokens.go
+++ Tokens.go
@@ -5,6 +5,7 @@ import (
 )
 
 const TOKEN_TYPE_ACTIVATION = "Token::Activation"
+const TOKEN_TYPE_REFRESH = "Token::Refresh"
 
 type Tokens struct {
     Id       uint   `gorm:"primary_key"`
@@ -12,6 +13,8 @@ type Tokens struct {
     MemberId uint
     IsUsed   bool
     Type     string `gorm:"size:255"`
+    ReqIp    string `gorm:"size:255"`
+    ReqData  string `sql:"type: text"`
 
     ExpiresAt *time.Time `sql:"default: null"`
     CreatedAt *time.Time `sql:"default: null"`
