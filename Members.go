4a2bf2eb1dae1ea2834aa9fdc7feba2d85242aeb
ihor <for0work0@gmail.com>
2018-01-25 13:32:16 +0200

    phone 2fa added

--- Members.go
+++ Members.go
@@ -11,19 +11,20 @@ const SN_PREFIX = "PEA"
 const SN_POSTFIX = "TIO"
 
 type Members struct {
-    Id          uint   `gorm:"primary_key"`
-    Sn          string `gorm:"size:255"`
-    DisplayName string `gorm:"size:255" sql:"default: null"`
-    Email       string `gorm:"size:255"`
-    IdentityId  uint   `sql:"default: null"`
-    State       uint   `sql:"default: null"`
-    Activated   bool   `sql:"default: null"`
-    CountryCode uint   `sql:"default: null"`
-    PhoneNumber string `gorm:"size:255" sql:"default: null"`
-    Disabled    bool
-    ApiDisabled bool
-    Nickname    string `gorm:"size:255" sql:"default: null"`
-    RefId       uint   `sql:"default: null"`
+    Id               uint   `gorm:"primary_key"`
+    Sn               string `gorm:"size:255"`
+    DisplayName      string `gorm:"size:255" sql:"default: null"`
+    Email            string `gorm:"size:255"`
+    IdentityId       uint   `sql:"default: null"`
+    State            uint   `sql:"default: null"`
+    Activated        bool   `sql:"default: null"`
+    CountryCode      uint   `sql:"default: null"`
+    PhoneNumber      string `gorm:"size:255" sql:"default: null"`
+    IsPhoneConfirmed bool
+    Disabled         bool
+    ApiDisabled      bool
+    Nickname         string `gorm:"size:255" sql:"default: null"`
+    RefId            uint   `sql:"default: null"`
 
     CreatedAt *time.Time
     UpdatedAt *time.Time
