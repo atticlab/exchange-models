22e569e371c7ce3240fae2b12f9878497dbe0444
Serhiy Korzun <kiberpunk.mail@gmail.com>
2018-01-26 16:34:33 +0200

    Txid -> TxHash

--- WithdrawsTransactions.go
+++ WithdrawsTransactions.go
@@ -5,14 +5,14 @@ import (
 )
 
 const (
-    WITH_TX_STATE_CREATED = "Created"
+    WITH_TX_STATE_CREATED    = "Created"
     WITH_TX_STATE_PROCESSING = "Processing"
-    WITH_TX_STATE_DONE = "Done"
+    WITH_TX_STATE_DONE       = "Done"
 )
 
 type WithdrawsTransactions struct {
     Id         uint    `gorm:"primary_key"`
-    Txid       string  `gorm:"size:255" sql:"default: null"`
+    TxHash     string  `gorm:"size:255" sql:"default: null"`
     State      string  `gorm:"size:255" sql:"default: null"`
     Fee        float64 `sql:"type:decimal(32,16);"`
     Txbody     string  `sql:"type: text"`
