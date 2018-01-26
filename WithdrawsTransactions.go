13d368ad71e9d18718e80646344e3f8578b70c68
Serhiy Korzun <kiberpunk.mail@gmail.com>
2018-01-26 15:02:13 +0200

    Added TX state

--- WithdrawsTransactions.go
+++ WithdrawsTransactions.go
@@ -7,6 +7,7 @@ import (
 type WithdrawsTransactions struct {
     Id         uint    `gorm:"primary_key"`
     Txid       string  `gorm:"size:255" sql:"default: null"`
+    State      string  `gorm:"size:255" sql:"default: null"`
     Fee        float64 `sql:"type:decimal(32,16);"`
     Txbody     string  `sql:"type: text"`
     TxbodyHash string  `gorm:"size:255" sql:"default: null"`
