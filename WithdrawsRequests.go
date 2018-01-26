83356a0542a1a17ad1d5baa983d39b2cba653c91
Serhiy Korzun <kiberpunk.mail@gmail.com>
2018-01-26 15:31:28 +0200

    With. Req. Type -> Address

--- WithdrawsRequests.go
+++ WithdrawsRequests.go
@@ -6,7 +6,7 @@ import (
 
 const (
     WITH_REQ_STATE_CREATED = "Created"
-    WITH_REQ_STATE_DONE = "Done"
+    WITH_REQ_STATE_DONE    = "Done"
 )
 
 type WithdrawsRequests struct {
@@ -14,11 +14,11 @@ type WithdrawsRequests struct {
     AccountId    uint
     MemberId     uint
     Currency     uint8
+    Address      string  `gorm:"size:255" sql:"default: null"`
     Amount       float64 `sql:"type:decimal(32,16);"`
     Fee          float64 `sql:"type:decimal(32,16);"`
     WithdrawTxId uint
     State        string  `gorm:"size:255" sql:"default: null"`
-    Type         string  `gorm:"size:255" sql:"default: null"`
 
     CreatedAt *time.Time `sql:"default: null"`
     UpdatedAt *time.Time `sql:"default: null"`
