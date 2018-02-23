2674ec1d2bf9bab109cc7d067452a6ac8d619709
Serhiy Korzun <kiberpunk.mail@gmail.com>
2018-02-23 12:10:03 +0200

    Updated to prod values of currencies

--- Currencies.go
+++ Currencies.go
@@ -18,16 +18,7 @@ type Currency struct {
 
 var AcceptedCurrencies = []Currency{
     {
-        Id:        1,
-        Title:     "Yuan",
-        Code:      "CNY",
-        Unit:      "Yuan",
-        UnitRatio: decimal.NewFromFloat(1),
-        IsCoin:    false,
-        IsToken:   false,
-    },
-    {
-        Id:            2,
+        Id:            1,
         Title:         "Bitcoin",
         Code:          "BTC",
         Unit:          "Satoshi",
@@ -37,17 +28,7 @@ var AcceptedCurrencies = []Currency{
         Confirmations: 3,
     },
     {
-        Id:            3,
-        Title:         "Ripple",
-        Code:          "XRP",
-        Unit:          "ripple",
-        UnitRatio:     decimal.NewFromFloat(math.Pow10(0)), //1.00 XRP ???
-        IsCoin:        true,
-        IsToken:       false,
-        Confirmations: 3,
-    },
-    {
-        Id:            4,
+        Id:            2,
         Title:         "Ethereum",
         Code:          "ETH",
         Unit:          "WEI",
